666839655a5879d45782903e425c5f950bca298a
maytawee <maytaj97@orcsf.postbox.in.th>
2021-04-19 20:45:17 +0700

    edit min sdk and UI

--- app/src/main/java/com/example/beuon/MainActivity.kt
+++ app/src/main/java/com/example/beuon/MainActivity.kt
@@ -8,18 +8,31 @@ import android.widget.TextView
 class MainActivity : AppCompatActivity() {
 
     var _usernameText: TextView? = null
+    private var roleUser: String = ""
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(com.example.beuon.R.layout.activity_main)
-        getUsername()
+        roleUser = this.getRoleUser()
     }
 
-    fun getUsername(){
+    fun getUsername() : String {
         val i = intent
         val username = i.getStringExtra("username")
         _usernameText = findViewById(R.id.text_username) as TextView
         _usernameText!!.setText("Welcome "+username.toString())
+        return username.toString()
+    }
+
+    private fun getRoleUser() : String{
+        if(getUsername().equals("studenttest")){
+            return "student"
+        }else if(getUsername().equals("tutortest")){
+            return "tutor"
+        }else if(getUsername().equals("parenttest")){
+            return "parents"
+        }
+        return ""
     }
 
 }
